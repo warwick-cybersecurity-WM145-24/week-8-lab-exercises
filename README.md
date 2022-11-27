a55c2ad25d8f0265a095874bb35178d84a09ba2a
Jujhar Singh <jujhar@jujhar.com>
2022-11-27 20:46:26 +0000

    adds a mock api in

---
+++ README.md
@@ -0,0 +1,14 @@
+# Week 8 and 9 lab exercises
+
+
+## Mock api content server
+
+to run
+```bash
+
+# install dependencies
+(cd mockeserver && npm install )
+
+# to run
+(cd mockeserver && npm start)
+```
--- mockserver/routes/index.js
+++ mockserver/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (request, response) => {
+  response.json({ title: "our cool content api", status: "all ok" });
+});
+
+/**
+ * Get an article based on a random ID
+ */
+router.get('/article/:id', (request, response) => {
+  const articleId = parseInt(request.params?.id);
+  response.json({ status: "your article", articleId });
+});
+
+/**
+ * Create a new article
+ */
+router.post('/article', (request, response) => {
+  response.statusCode = 201;
+  response.json({ status: "Thanks for the new article" });
 });
 
 module.exports = router;
